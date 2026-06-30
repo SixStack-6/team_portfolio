8bb9d20fe50bd5d26d0c3d7ef2d69947fd0633be
Shruti singh <shrutising025@gmail.com>
2026-06-30 15:08:21 +0530

    Fix photo file name format for team member

--- js/data.js
+++ js/data.js
@@ -26,7 +26,7 @@ const teamMembers = [
     role: 'Frontend Developer',
     skills: ['HTML', 'CSS', 'JavaScript'],
     github: 'https://github.com/ShrutiNOVA',
-    photo: 'assets/team/WhatsApp Image 2026-03-17 at 22.22.21.jpeg',
+    photo: 'assets/team/WhatsAppImage2026-03-17at22.22.21.jpeg',
   },
 ];
 
